ec8edc44213d1b834e88aa7c6ca8856ba5164a22
belcherj <belcherj@gmail.com>
2019-03-08 16:44:20 -0500

    Adds a G Suite stats banner

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -173,4 +173,14 @@ export default {
 		},
 		defaultVariation: 'original',
 	},
+	gSuiteStatsNudge: {
+		datestamp: '20190308',
+		variations: {
+			copy1: 25,
+			copy2: 25,
+			copy3: 25,
+			copy4: 25,
+		},
+		defaultVariation: 'copy1',
+	},
 };
